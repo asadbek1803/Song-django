e7caac52a33769a0141d036115d8538a7fcd99bb
asadbek <asadbekabdubannopov0408@gmail.com>
2023-09-09 10:22:12 +0500

    New models: Artist, Song, SongRecord, Album

--- app1/models.py
+++ app1/models.py
@@ -3,6 +3,46 @@ from django.db import models
 
 # Create your models here.
 
+
+class Artist(models.Model):
+    gender_a = (
+        ('male', 'Male'),
+        ('female','Female'),
+        ('other','Other')
+    )
+    profile_img = models.FileField(blank=True, null=True, upload_to='media/images')
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=16,choices=gender_a)
+    age = models.PositiveSmallIntegerField(default=25)
+    email = models.EmailField(default='example@gmail.com', blank=True, null=True)
+    phone = models.CharField(max_length=19)
+    about = models.TextField(default="Your about only", blank=True, null=True)
+    address = models.CharField(max_length=300)
+
 class Song(models.Model):
-    name = models.CharField(max_length=25)
+    janr = (
+        ('rap','Rap'),
+        ('jazz','Jazz'),
+        ('hiphop','HipHop'),
+        ('national','national'),
+        ('rock','Rock')
+    )
+    title = models.CharField(max_length=25)
     year = models.DateField(blank=True, null=True)
+    ganre = models.CharField(max_length=15, choices=janr)
+    artist = models.ManyToManyField(Artist, blank=True, null=True)
+
+
+class SongRecord(models.Model):
+    music = models.ForeignKey(Song, on_delete=models.CASCADE)
+    artsit = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=50)
+    album_img = models.FileField(blank=True, null=True, upload_to='media/album_image')
+    year = models.DateTimeField(default=2023)
+    song = models.ManyToManyField(Song, blank=True, null=True)
+    artist = models.ManyToManyField(Artist, blank=True, null=True)
+    album_about = models.TextField(blank=True, null=True, default='About your album')
+

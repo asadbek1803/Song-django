fe8611e06343653b201abce8f592397277bf4eb3
asadbek <asadbekabdubannopov0408@gmail.com>
2023-09-09 10:25:01 +0500

    Admin register models

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import Song, SongRecord, Artist, Album
 
 # Register your models here.
+
+
+admin.site.register(Song)
+admin.site.register(SongRecord)
+admin.site.register(Artist)
+admin.site.register(Album)
